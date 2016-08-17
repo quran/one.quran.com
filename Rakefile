e4707a5a0ad9b7a734372932634516f9bede5574
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-08-16 23:45:19 -0700

    get it to pass!

--- Rakefile
+++ Rakefile
@@ -5,7 +5,6 @@ require_relative 'config/application'
 
 Rails.application.load_tasks
 
-
 begin
   require 'rubocop/rake_task'
   RuboCop::RakeTask.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'oauth/facebook'
+      get 'oauth/create'
     end
   end
 
-  get 'auth/:provider/callback', to: 'api/v1/oauth#create'
+  get 'oauth/:provider/callback', to: 'api/v1/oauth#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
