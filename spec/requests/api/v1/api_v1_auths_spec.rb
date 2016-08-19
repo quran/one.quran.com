7a59f930c9db7af4b01ae5a3f0a484bfa706aa7c
Mohamed El Mahallawy <mmahalwy@gmail.com>
2016-08-18 21:58:19 -0700

    specs!

--- spec/requests/api/v1/api_v1_auths_spec.rb
+++ spec/requests/api/v1/api_v1_auths_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::AuthController, type: :controller do
+RSpec.describe 'Api::V1::Auth', type: :request do
   let(:algorithm) { 'HS256' }
   let(:user) { create(:user) }
   let(:token_payload) do
@@ -19,23 +19,21 @@ RSpec.describe Api::V1::AuthController, type: :controller do
 
   let(:jwt) { JWT.encode(token_payload, Rails.application.secrets.secret_key_base, algorithm) }
 
-  describe 'GET #current' do
+  describe 'GET /api/v1/auth/current' do
     it 'returns unauthorized when calling with no token' do
-      get :current
+      get api_v1_auth_current_path
       expect(response).to have_http_status(:unauthorized)
     end
 
     it 'returns unauthorized with invalud token' do
-      request.headers['Authorization'] = 'NOT_VALID'
-      get :current
+      get api_v1_auth_current_path, headers: { 'Authorization' => 'NOT_VALID' }
       expect(response).to have_http_status(:unauthorized)
     end
 
-    it 'returns success when token is valid' do
-      request.headers['Authorization'] = "Bearer #{jwt}"
-      get :current
+    it 'returns success when token is valid with the user' do
+      get api_v1_auth_current_path, headers: { 'Authorization' => "Bearer #{jwt}" }
       expect(response).to have_http_status(:success)
+      expect(response_json['user'].sort.to_h).to eql(user.as_json.except('id', 'created_at', 'updated_at', 'oauth_token', 'oauth_expires_at').sort.to_h)
     end
   end
-
 end
