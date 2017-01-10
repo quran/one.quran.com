cb0a371e10165a8eb2762e75ac46067033d4a3de
Mohamed El Mahallawy <mmahalwy@gmail.com>
2017-01-10 11:41:29 -0800

    token auth

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BookmarksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
   # GET /bookmarks
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  OmniAuth.config.full_host = "http://localhost:3030"
 end
